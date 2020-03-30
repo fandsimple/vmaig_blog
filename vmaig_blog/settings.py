d40327864e57b0286fe0ce2df7c658c0cee3f80f
fandsimple <fand1024@163.com>
2020-03-30 17:14:47 +0800

    add

--- vmaig_blog/settings.py
+++ vmaig_blog/settings.py
@@ -79,9 +79,18 @@ WSGI_APPLICATION = 'vmaig_blog.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vmaig_blog',
+        'USER': 'root',
+        'PASSWORD': '102487aa',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'CONN_MAX_AGE': 9
     }
 }
 
